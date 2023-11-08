4a13d08f81c3ad486577a659cae535e8e1a13077
kimyuyeonn <150135857+kimyuyeonn@users.noreply.github.com>
2023-11-09 08:24:54 +0900

    Add files via upload

---
+++ WEB/webcam.py
@@ -0,0 +1,49 @@
+from ultralytics import YOLO
+import cv2
+import math
+
+def video_detection(path_x):
+    video_capture = path_x
+    #Create a Webcam Object
+    cap=cv2.VideoCapture(video_capture)
+    frame_width=int(cap.get(3))
+    frame_height=int(cap.get(4))
+    #out=cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P','G'), 10, (frame_width, frame_height))
+
+    model=YOLO('../YOLO-PILL-weights/last.pt')
+    classNames = ['Minocin Cap. 50mg', 'Buspar Tab. 10mg Boryung', 'Dinacs Tab.', 'Tylenol Tab. 500mg',
+                 'Proscar Tab. 5mg', 'Beecom-CF Tab.', 'Joins Tab.', 'Ssiznal Tab.', 'Festal Plus Tab.',
+                 'Riboteine Tab.', 'Dr. Bearse Tab.', 'Isotinone Soft Cap.', 'Ebixa Tab.', 'Entelon Tab. 150mg',
+                 'Avodart Soft Cap. 0.5mg', 'Gabatopa Tab. 100mg', 'Omacor Soft Cap.', 'Rabiet Tab.20mg',
+                 'Allerdin Tab.', 'Neotigason Cap. 10mg', 'Cacepin Tab 25mg', 'Zeromine Soft Cap.',
+                 'Kanarb Tab. 60mg', 'Olzapine Tab. 2.5mg', 'Aspid Tab.', 'Hanmi Tams Cap. 0.4mg',
+                 'Entresto Film Coated Tab. 100mg', 'Agen-F Tab.', 'Ramnos Cap. 500mg', 'Haben Herb F Cap.',
+                 'Elsteine Tab.']
+
+    while True:
+        success, img = cap.read()
+        results=model(img,stream=True)
+        for r in results:
+            boxes=r.boxes
+            for box in boxes:
+                x1,y1,x2,y2=box.xyxy[0]
+                x1,y1,x2,y2=int(x1), int(y1), int(x2), int(y2)
+                print(x1,y1,x2,y2)
+                cv2.rectangle(img, (x1,y1), (x2,y2), (255,0,255),3)
+                conf=math.ceil((box.conf[0]*100))/100
+                cls=int(box.cls[0])
+                class_name=classNames[cls]
+                label=f'{class_name}{conf}'
+                t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                print(t_size)
+                c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                cv2.rectangle(img, (x1,y1), c2, [255,0,255], -1, cv2.LINE_AA)  # filled
+                cv2.putText(img, label, (x1,y1-2),0, 1,[255,255,255], thickness=1,lineType=cv2.LINE_AA)
+
+        yield img
+        #out.write(img)
+        #cv2.imshow("image", img)
+        #if cv2.waitKey(1) & 0xFF==ord('1'):
+            #break
+    #out.release()
+cv2.destroyAllWindows()
